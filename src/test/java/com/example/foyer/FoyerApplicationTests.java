25796706c7d3cd250445b746664fa08cdb327b72
chaouch2 <78444962+chaouch2@users.noreply.github.com>
2025-07-17 02:06:49 +0200

    Update FoyerApplicationTests.java

--- src/test/java/com/example/foyer/FoyerApplicationTests.java
+++ src/test/java/com/example/foyer/FoyerApplicationTests.java
@@ -1,8 +1,13 @@
 package com.example.foyer;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
+@SpringBootTest
+@ActiveProfiles("test") // 👉 utilise application-test.properties
 class FoyerApplicationTests {
 
     @Test
@@ -10,5 +15,4 @@ class FoyerApplicationTests {
         // Test simple qui passe toujours
         assertTrue(true, "Le test contextLoads passe toujours");
     }
-
 }
