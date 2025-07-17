b35be950805fd390e14430a5dc9e886874cf9dd4
chaouch2 <78444962+chaouch2@users.noreply.github.com>
2025-07-17 02:19:08 +0200

    Update FoyerApplicationTests.java

--- src/test/java/com/example/foyer/FoyerApplicationTests.java
+++ src/test/java/com/example/foyer/FoyerApplicationTests.java
@@ -1,10 +1,9 @@
+package com.example.foyer;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.assertTrue;
-
 class FoyerApplicationTests {
-
     @Test
     void contextLoads() {
-        assertTrue(true);
+        assertTrue(true, "Le test contextLoads passe toujours");
     }
 }
